b68ae88ad625506663c0cf180331e1833b63bb63
Alistair Smith <alistairsmith01@gmail.com>
2021-08-06 01:47:31 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['xo', 'xo-typescript'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/comma-dangle': 'off',
+		'@typescript-eslint/triple-slash-reference': 'off',
+		'import/no-anonymous-default-export': 'off',
+		'@typescript-eslint/indent': 'off',
+	},
+	ignorePatterns: ['**/*.js'],
+};
