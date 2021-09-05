bf5f6173d2959201b6f048dbbae95d8924df228c
Alistair Smith <alistairsmith01@gmail.com>
2021-09-05 03:31:40 +0100

    true

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 type Listener<P extends Record<string, unknown>, K extends keyof P> = (
-	event: SakutaEvent<P, K>
+	event: SakutaEvent<K, P>
 ) => unknown;
 
-export class Sakuta<Payloads extends Record<string, unknown>> {
-	private readonly listeners: Array<Listener<keyof Payloads, Payloads[keyof Payloads]>> = [];
+export class Sakuta<Key extends string, Payloads extends Record<Key, unknown>> {
+	private readonly listeners: Array<Listener<Payloads, keyof Payloads>> = [];
 
 	on(key: Key) {
-		//
+		console.log(key);
 	}
 }
 
-class SakutaEvent<P extends Record<string, unknown>, K extends keyof P> {
-	public readonly instance: Sakuta<P>;
+class SakutaEvent<K extends string, P extends Record<K, unknown>> {
+	public readonly instance: Sakuta<K, P>;
 	public readonly key: K;
 	public readonly data: P[K];
 
-	constructor(instance: Sakuta<P>, key: K, data: P[K]) {
+	constructor(instance: Sakuta<K, P>, key: K, data: P[K]) {
 		this.instance = instance;
 		this.key = key;
 		this.data = data;
