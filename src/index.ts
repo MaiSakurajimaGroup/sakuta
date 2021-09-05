cf348dfa76890929f0ce03dac071f6e05a010c00
Alistair Smith <alistairsmith01@gmail.com>
2021-09-05 04:33:04 +0100

    NICE TYPE SAFEYY

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-type Listener<P extends Record<string, unknown>, K extends keyof P> = (
-	event: SakutaEvent<K, P>
+type Listener<P extends Record<string, unknown>, Key extends keyof P> = (
+	event: SakutaEvent<P, Key>
 ) => unknown;
-
-export class Sakuta<Key extends string, Payloads extends Record<Key, unknown>> {
+export class Sakuta<Payloads extends Record<string, unknown>> {
 	private readonly listeners: Array<Listener<Payloads, keyof Payloads>> = [];
 
-	on(key: Key) {
+	on(key: keyof Payloads) {
 		console.log(key);
 	}
 }
 
-class SakutaEvent<K extends string, P extends Record<K, unknown>> {
-	public readonly instance: Sakuta<K, P>;
+class SakutaEvent<P extends Record<string, unknown>, K extends keyof P> {
+	public readonly instance: Sakuta<P>;
 	public readonly key: K;
 	public readonly data: P[K];
 
-	constructor(instance: Sakuta<K, P>, key: K, data: P[K]) {
+	constructor(instance: Sakuta<P>, key: K, data: P[K]) {
 		this.instance = instance;
 		this.key = key;
 		this.data = data;
